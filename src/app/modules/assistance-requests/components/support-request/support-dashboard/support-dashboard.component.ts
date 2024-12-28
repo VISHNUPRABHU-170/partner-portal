5a0d2ab03eb25f6f7efc9bbf08b01d5b667a107b
VISHNUPRABHU-170 <vishnurr1708@gmail.com>
2024-12-28 13:18:06 +0530

    Updated Support Request Dashboard.

--- src/app/modules/assistance-requests/components/support-request/support-dashboard/support-dashboard.component.ts
+++ src/app/modules/assistance-requests/components/support-request/support-dashboard/support-dashboard.component.ts
@@ -1,23 +1,80 @@
-import { Component } from '@angular/core';
+import { SupportRequestService } from './../../../services/support-request/support-request.service';
+import { Component, DestroyRef } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { createRequestButtonConfig } from './config';
 import { ButtonComponent } from '../../../../core/components/button/button.component';
 import { NavigationService } from '../../../../core/services/navigation/navigation.service';
 import { ButtonComponentModel } from '../../../../core/components/button/button.component.model';
+import { PieChartComponent } from '../../../../core/components/pie-chart/pie-chart.component';
+import { PaginatorComponent } from "../../../../core/components/paginator/paginator.component";
+import { TableComponent } from "../../../../core/components/table/table.component";
+import { SupportTicketModel } from '../../../models/support-ticket.model';
+import { PageEvent } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-support-dashboard',
   standalone: true,
-  imports: [MatToolbarModule, ButtonComponent],
+  imports: [MatToolbarModule, ButtonComponent, PieChartComponent, PaginatorComponent, TableComponent],
   templateUrl: './support-dashboard.component.html',
   styleUrl: './support-dashboard.component.scss'
 })
 export class SupportDashboardComponent {
   createRequestButtonConfig = createRequestButtonConfig;
 
-  constructor (private navigationService: NavigationService) { }
+  tableData: SupportTicketModel[] = [];
+  columnsDef = ['title', 'description', 'cloudProvider', 'priority', 'deadLine'];
+
+  pageIndex: number = 0;
+  totalTickets: number = 0;
+
+  constructor (
+    private navigationService: NavigationService,
+    private supportRequestService: SupportRequestService,
+    private destroyRef: DestroyRef
+  ) { }
+
+  ngOnInit(): void {
+    this.supportRequestService.getTicketStatus();
+    this.supportRequestService.getTickets({ page: this.pageIndex, limit: 5 });
+    this.subscribeToFeatureTickets();
+    this.subscribeToFeatureTicketStatus();
+  }
+
+  subscribeToFeatureTicketStatus() {
+    const Subscription = this.supportRequestService.ticketStatusBehaviorSubject.subscribe((response: any) => {
+      this.updateChartConfig(response);
+    });
+    this.destroyRef.onDestroy(() => {
+      Subscription?.unsubscribe();
+    });
+  }
+
+  subscribeToFeatureTickets() {
+    const Subscription = this.supportRequestService.ticketsBehaviorSubject.subscribe((data: any) => {
+      this.tableData = data?.tickets;
+      this.totalTickets = data?.totalTickets;
+    });
+    this.destroyRef.onDestroy(() => {
+      Subscription?.unsubscribe();
+    });
+  }
+
+  updateChartConfig(ticketStatus: any) {
+
+  }
 
   onCreateRequest(data: ButtonComponentModel) {
     this.navigationService.navigate(data.routerLink);
+  }
+
+  onTableEvent(data: any) {
+    const queryParams = { id: data._id };
+    this.navigationService.navigate('/partner-portal/assistance-requests/feature-ticket-view', queryParams);
+  }
+
+  onPaginatorEvent(eve: PageEvent) {
+    this.tableData = [];
+    this.pageIndex = eve.pageIndex;
+    this.supportRequestService.getTickets({ page: this.pageIndex, limit: 5 });
   }
 }
--- src/app/modules/assistance-requests/models/support-ticket.model.ts
+++ src/app/modules/assistance-requests/models/support-ticket.model.ts
@@ -1,9 +1,6 @@
-import { CloudProviders } from "./feature-ticket.model";
-
 export interface SupportTicketModel {
   title: string;
   description: string;
-  cloudProvider: CloudProviders;
   priority: string;
   deadLine: string;
   fileUpload: string;
