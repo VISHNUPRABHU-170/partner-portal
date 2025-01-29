9a4a408529599af7c64b1caa18e90a92f780e3ff
VISHNUPRABHU-170 <vishnurr1708@gmail.com>
2025-01-29 22:00:54 +0530

    Implemented Update button for Tickets.

--- src/app/modules/assistance-requests/components/feature-request/feature-ticket-view/feature-ticket-view.component.ts
+++ src/app/modules/assistance-requests/components/feature-request/feature-ticket-view/feature-ticket-view.component.ts
@@ -1,24 +1,26 @@
 import { Component, DestroyRef, OnInit } from '@angular/core';
 import { MatToolbar } from '@angular/material/toolbar';
 import { ActivatedRoute } from '@angular/router';
-import { backIconConfig, progressBarConfig } from './config';
+import { backIconConfig, progressBarConfig, updateButtonConfig } from './config';
 import { IconComponent } from '../../../../core/components/icon/icon.component';
 import { FeatureRequestService } from '../../../services/feature-request/feature-request.service';
 import { ProgressBarComponent } from '../../../../core/components/progress-bar/progress-bar.component';
 import { MatCardModule } from '@angular/material/card';
 import { FeatureTicketModel } from '../../../models/feature-ticket.model';
 import { DatePipe } from '@angular/common';
+import { ButtonComponent } from '../../../../core/components/button/button.component';
 
 @Component({
   selector: 'app-feature-ticket-view',
   standalone: true,
-  imports: [MatToolbar, MatCardModule, IconComponent, ProgressBarComponent, DatePipe],
+  imports: [MatToolbar, MatCardModule, IconComponent, ProgressBarComponent, DatePipe, ButtonComponent],
   templateUrl: './feature-ticket-view.component.html',
   styleUrl: './feature-ticket-view.component.scss',
 })
 export class FeatureTicketViewComponent implements OnInit {
   backIconConfig = backIconConfig;
   progressBarConfig = progressBarConfig;
+  updateButtonConfig = updateButtonConfig;
 
   ticketData!: FeatureTicketModel;
 
@@ -38,5 +40,8 @@ export class FeatureTicketViewComponent implements OnInit {
     this.destroyRef.onDestroy(() => {
       Subscription?.unsubscribe();
     });
+  }
+
+  onUpdate(): void {
   }
 }
--- src/app/modules/assistance-requests/components/support-request/support-ticket-view/support-ticket-view.component.ts
+++ src/app/modules/assistance-requests/components/support-request/support-ticket-view/support-ticket-view.component.ts
@@ -1,3 +1,4 @@
+import { updateButtonConfig } from './../../feature-request/feature-ticket-view/config';
 import { SupportRequestService } from './../../../services/support-request/support-request.service';
 import { Component, DestroyRef, OnInit } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
@@ -8,17 +9,19 @@ import { backIconConfig, previewLinkConfig, progressBarConfig } from './config';
 import { ActivatedRoute } from '@angular/router';
 import { SupportTicketModel } from '../../../models/support-ticket.model';
 import { DatePipe } from '@angular/common';
+import { ButtonComponent } from '../../../../core/components/button/button.component';
 
 @Component({
   selector: 'app-support-ticket-view',
   standalone: true,
-  imports: [MatToolbar, MatCardModule, IconComponent, ProgressBarComponent, DatePipe],
+  imports: [MatToolbar, MatCardModule, IconComponent, ProgressBarComponent, DatePipe, ButtonComponent],
   templateUrl: './support-ticket-view.component.html',
   styleUrl: './support-ticket-view.component.scss',
 })
 export class SupportTicketViewComponent implements OnInit {
   backIconConfig = backIconConfig;
   progressBarConfig = progressBarConfig;
+  updateButtonConfig = updateButtonConfig;
   previewLinkConfig = previewLinkConfig;
 
   ticketData!: SupportTicketModel;
@@ -42,6 +45,10 @@ export class SupportTicketViewComponent implements OnInit {
   }
 
   onPreview() {
+    // TODO
+  }
+
+  onUpdate(): void {
     // TODO
   }
 }
